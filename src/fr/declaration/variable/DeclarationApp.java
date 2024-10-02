07b5fcdcde3031f4fc1aa368525a4feafdac80ef
Cooljeb <joseph.brou@gmail.com>
2024-10-02 12:21:45 +0200

    Tp02

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,22 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+    public static void main(String[] args) {
+
+        byte monByte = 1;
+        short monShort = 20;
+        int monInt = 1452010;
+        long monLong = 92233720368547758L;
+        float monFloat = 78954;
+        double monDouble = 789545.45;
+        char monChar = 'a';
+        boolean monBoolean = true;
+        String monString = "Java";
+        String randomString = "Voici le résultat d’un calcul :\n 1+5=6";
+
+        System.out.println(monByte);
+        System.out.println(randomString);
+    }
+}
+
